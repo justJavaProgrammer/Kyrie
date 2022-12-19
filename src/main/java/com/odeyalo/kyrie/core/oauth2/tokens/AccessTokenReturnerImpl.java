0ac571b433769d853ed29c97a4f2308cd9368025
Odeyalo <theprogrammerinfuture@gmail.com>
2022-12-19 13:47:25 +0200

    Return access token by authorization code

--- src/main/java/com/odeyalo/kyrie/core/oauth2/tokens/AccessTokenReturnerImpl.java
+++ src/main/java/com/odeyalo/kyrie/core/oauth2/tokens/AccessTokenReturnerImpl.java
@@ -1,42 +1,39 @@
 package com.odeyalo.kyrie.core.oauth2.tokens;
 
-import com.odeyalo.kyrie.core.User;
+import com.odeyalo.kyrie.core.Oauth2User;
 import com.odeyalo.kyrie.core.oauth2.client.ClientCredentialsValidator;
 import com.odeyalo.kyrie.core.oauth2.client.ValidationResult;
 import com.odeyalo.kyrie.core.oauth2.tokens.code.AuthorizationCode;
 import com.odeyalo.kyrie.core.oauth2.tokens.code.AuthorizationCodeManager;
-import com.odeyalo.kyrie.core.oauth2.tokens.jwt.JwtTokenMetadata;
-import com.odeyalo.kyrie.core.oauth2.tokens.jwt.JwtTokenProvider;
-import com.odeyalo.kyrie.dto.TokensResponse;
+import com.odeyalo.kyrie.core.oauth2.tokens.jwt.Oauth2AccessTokenGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Set;
 
 @Service
 public class AccessTokenReturnerImpl implements AccessTokenReturner {
     private final ClientCredentialsValidator clientCredentialsValidator;
     private final AuthorizationCodeManager authorizationCodeManager;
-    private final JwtTokenProvider tokenProvider;
+    private final Oauth2AccessTokenGenerator generator;
 
     @Autowired
-    public AccessTokenReturnerImpl(ClientCredentialsValidator clientCredentialsValidator, AuthorizationCodeManager authorizationCodeManager, JwtTokenProvider tokenProvider) {
+    public AccessTokenReturnerImpl(ClientCredentialsValidator clientCredentialsValidator, AuthorizationCodeManager authorizationCodeManager, Oauth2AccessTokenGenerator generator) {
         this.clientCredentialsValidator = clientCredentialsValidator;
         this.authorizationCodeManager = authorizationCodeManager;
-        this.tokenProvider = tokenProvider;
+        this.generator = generator;
     }
 
     @Override
-    public TokensResponse getToken(String clientId, String clientSecret, String authorizationCode) throws ObtainTokenException {
+    public Oauth2AccessToken getToken(String clientId, String clientSecret, String authorizationCode) throws ObtainTokenException {
         ValidationResult validationResult = clientCredentialsValidator.validateCredentials(clientId, clientSecret);
         if (!validationResult.isSuccess()) {
-            throw new ObtainTokenException("Client credentials is wrong and cannot be used to obtain the access token");
+            throw new ObtainTokenException("Client credentials is wrong and cannot be used to obtain an access token");
         }
         AuthorizationCode authCode = authorizationCodeManager.getAuthorizationCodeByAuthorizationCodeValue(authorizationCode);
         if (authCode == null || authCode.isExpired()) {
             throw new ObtainTokenException("The authorization code does not found or expired");
         }
-        JwtTokenMetadata metadata = tokenProvider.generateJwtToken(new User("odeyalo", "password", Set.of("USER")), new String[]{"read"});
-        return TokensResponse.builder().token(metadata.getToken()).expiresIn(metadata.getExpiresIn()).prefix(metadata.getPrefix()).scopes(metadata.getScopes()).build();
+        Oauth2User user = authCode.getUser();
+        String[] scopes = authCode.getScopes();
+        return generator.generateAccessToken(user, scopes);
     }
 }
