09e2e6e98d530aac55f9b21516761c23a9e21b40
Odeyalo <theprogrammerinfuture@gmail.com>
2022-12-19 13:47:21 +0200

    Return access token by authorization code

--- src/main/java/com/odeyalo/kyrie/core/oauth2/tokens/AccessTokenReturner.java
+++ src/main/java/com/odeyalo/kyrie/core/oauth2/tokens/AccessTokenReturner.java
@@ -1,6 +1,6 @@
 package com.odeyalo.kyrie.core.oauth2.tokens;
 
-import com.odeyalo.kyrie.dto.TokensResponse;
+import com.odeyalo.kyrie.core.oauth2.Oauth2ClientCredentials;
 
 /**
  * Return access token by authorization code
@@ -14,6 +14,10 @@ public interface AccessTokenReturner {
      * @param authorizationCode - authorization code that client got from '/oauth/authorize' endpoint
      * @return - TokensResponse with all fields included
      */
-    TokensResponse getToken(String clientId, String clientSecret, String authorizationCode) throws ObtainTokenException;
+    Oauth2AccessToken getToken(String clientId, String clientSecret, String authorizationCode) throws ObtainTokenException;
 
+
+    default Oauth2AccessToken getToken(Oauth2ClientCredentials credentials, String authorizationCode) throws ObtainTokenException {
+        return getToken(credentials.getClientId(), credentials.getClientSecret(), authorizationCode);
+    }
 }
