05bc7c20c06418ae5393d201bc76939b89643cc9
Odeyalo <theprogrammerinfuture@gmail.com>
2022-12-19 14:03:20 +0200

    Default ClientSideOauth2FlowHandlerFactory that produces only created implementations of ClientSideOauth2FlowHandler type based on AuthorizationRequest grant type

---
+++ src/main/java/com/odeyalo/kyrie/core/oauth2/flow/DefaultClientSideOauth2FlowHandlerFactory.java
@@ -0,0 +1,38 @@
+package com.odeyalo.kyrie.core.oauth2.flow;
+
+import com.odeyalo.kyrie.core.authorization.AuthorizationRequest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/**
+ * Default ClientSideOauth2FlowHandlerFactory that produces only created implementations of
+ * ClientSideOauth2FlowHandler type based on AuthorizationRequest grant type;
+ * @see ClientSideOauth2FlowHandler
+ * @see ClientSideOauth2FlowHandlerFactory
+ * @see AuthorizationRequest
+ * @version 1.0
+ */
+@Component
+public class DefaultClientSideOauth2FlowHandlerFactory implements ClientSideOauth2FlowHandlerFactory {
+    private final Map<String, ClientSideOauth2FlowHandler> handlers;
+    private final Logger logger = LoggerFactory.getLogger(DefaultClientSideOauth2FlowHandlerFactory.class);
+
+    @Autowired
+    public DefaultClientSideOauth2FlowHandlerFactory(List<ClientSideOauth2FlowHandler> handlers) {
+        this.handlers = handlers.stream().collect(Collectors.toMap(Oauth2FlowHandler::getFlowName, Function.identity()));
+        this.logger.info("Initialized the handlers map with: {}", handlers);
+    }
+
+    @Override
+    public ClientSideOauth2FlowHandler getHandler(AuthorizationRequest request) {
+        String grantType = request.getGrantType().getGrantName();
+        return handlers.get(grantType);
+    }
+}
