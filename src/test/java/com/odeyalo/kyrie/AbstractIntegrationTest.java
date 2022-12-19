5673191a6469c404f4431b3aced6c2a90bf5deeb
Odeyalo <theprogrammerinfuture@gmail.com>
2022-12-19 22:08:34 +0200

    Used to cache Spring application context and avoid context recreating to boost performance

---
+++ src/test/java/com/odeyalo/kyrie/AbstractIntegrationTest.java
@@ -0,0 +1,10 @@
+package com.odeyalo.kyrie;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Used to cache Spring application context and avoid context recreating to boost performance
+ */
+@SpringBootTest
+public class AbstractIntegrationTest {
+}
