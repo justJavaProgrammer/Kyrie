d96849d04c87d56021a3137f88ad736e485819ec
Odeyalo <theprogrammerinfuture@gmail.com>
2022-12-19 14:00:23 +0200

    Interface to validate provided client's credentials

---
+++ src/main/java/com/odeyalo/kyrie/core/oauth2/client/ClientCredentialsValidator.java
@@ -0,0 +1,21 @@
+package com.odeyalo.kyrie.core.oauth2.client;
+
+import com.odeyalo.kyrie.core.oauth2.Oauth2ClientCredentials;
+
+/**
+ * Interface to validate provided client's credentials
+ */
+public interface ClientCredentialsValidator {
+
+    /**
+     * Validate client credentials
+     * @param clientId - client id
+     * @param clientSecret - client secret
+     * @return - ValidationResult
+     */
+    ValidationResult validateCredentials(String clientId, String clientSecret);
+
+    default ValidationResult validateCredentials(Oauth2ClientCredentials credentials) {
+        return validateCredentials(credentials.getClientId(), credentials.getClientSecret());
+    }
+}
