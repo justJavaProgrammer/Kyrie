cb8a4acd907c4a629f3728f909ff4ad5109e7a40
Odeyalo <theprogrammerinfuture@gmail.com>
2022-12-20 15:16:41 +0200

    Web mvc configuration that add custom resource handlers, arguments resolvers, etc

--- src/main/java/com/odeyalo/kyrie/config/MvcConfiguration.java
+++ src/main/java/com/odeyalo/kyrie/config/MvcConfiguration.java
@@ -1,9 +1,12 @@
 package com.odeyalo.kyrie.config;
 
 import com.odeyalo.kyrie.controllers.support.AdvancedModelAttributeMethodProcessor;
+import com.odeyalo.kyrie.controllers.support.SpaceSeparatedStringToArrayConverter;
 import com.odeyalo.kyrie.controllers.support.String2AuthorizationGrantTypeConverter;
 import com.odeyalo.kyrie.controllers.support.String2ResponseTypeConverter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.support.DefaultConversionService;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.method.annotation.ModelAttributeMethodProcessor;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -22,12 +25,12 @@ public class MvcConfiguration implements WebMvcConfigurer {
             "classpath:/META-INF/resources/", "classpath:/resources/",
             "classpath:/static/", "classpath:/public/" };
 
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**")
                 .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
     }
-
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
@@ -36,7 +39,26 @@ public class MvcConfiguration implements WebMvcConfigurer {
 
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        registry.addConverter(new String2AuthorizationGrantTypeConverter());
-        registry.addConverter(new String2ResponseTypeConverter());
+        registry.addConverter(string2AuthorizationGrantTypeConverter());
+        registry.addConverter(string2ResponseTypeConverter());
+        registry.addConverter(new SpaceSeparatedStringToArrayConverter(defaultConversionService()));
+    }
+
+    @Bean
+    public DefaultConversionService defaultConversionService() {
+        DefaultConversionService defaultConversionService = new DefaultConversionService();
+        defaultConversionService.addConverter(string2ResponseTypeConverter());
+        defaultConversionService.addConverter(string2AuthorizationGrantTypeConverter());
+        return defaultConversionService;
+    }
+
+    @Bean
+    public String2AuthorizationGrantTypeConverter string2AuthorizationGrantTypeConverter() {
+        return new String2AuthorizationGrantTypeConverter();
+    }
+
+    @Bean
+    public String2ResponseTypeConverter string2ResponseTypeConverter() {
+        return new String2ResponseTypeConverter();
     }
 }
