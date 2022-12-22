e8ce9e7edbfca64cf8b1a457a31cc50935b4090d
Odeyalo <theprogrammerinfuture@gmail.com>
2022-12-22 20:06:22 +0200

    Added constructor with JsonCreator

--- src/main/java/com/odeyalo/kyrie/dto/KyrieSuccessfulObtainTokenResponse.java
+++ src/main/java/com/odeyalo/kyrie/dto/KyrieSuccessfulObtainTokenResponse.java
@@ -1,18 +1,22 @@
 package com.odeyalo.kyrie.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
 
 /**
  * Represents successful access token response and add the additional properties such id token
+ *
  * @see <a href="https://www.rfc-editor.org/rfc/rfc6749#section-4.1.4">Successful access token response</a>
  * @see <a href="https://openid.net/specs/openid-connect-core-1_0.html#TokenResponse">Successful Token Response</a> by OpenID specification
  */
 @Data
 @Builder
 @AllArgsConstructor
-@RequiredArgsConstructor
 public class KyrieSuccessfulObtainTokenResponse {
     // Token value
     @JsonProperty("access_token")
@@ -32,4 +36,12 @@ public class KyrieSuccessfulObtainTokenResponse {
     @JsonProperty("id_token")
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private String idToken;
+
+    @JsonCreator
+    public KyrieSuccessfulObtainTokenResponse(@NonNull String token, @NonNull String prefix, @JsonProperty("expires_in") @NonNull Long expiresIn, @NonNull String scopes) {
+        this.token = token;
+        this.prefix = prefix;
+        this.expiresIn = expiresIn;
+        this.scopes = scopes;
+    }
 }
