3e89702281ce705355682b09b9d447ee68eb4e29
Odeyalo <theprogrammerinfuture@gmail.com>
2022-12-22 20:08:03 +0200

    Custom Converter to convert String to Oauth2ResponseType

--- src/main/java/com/odeyalo/kyrie/controllers/support/String2ResponseTypeConverter.java
+++ src/main/java/com/odeyalo/kyrie/controllers/support/String2ResponseTypeConverter.java
@@ -1,15 +1,22 @@
 package com.odeyalo.kyrie.controllers.support;
 
-import com.odeyalo.kyrie.core.authorization.ResponseType;
+import com.odeyalo.kyrie.core.authorization.Oauth2ResponseType;
+import com.odeyalo.kyrie.core.oauth2.oidc.OidcResponseType;
 import org.springframework.core.convert.converter.Converter;
 
 /**
- * Convert string to ResponseType
+ * Custom Converter to convert String to Oauth2ResponseType
+ * @see Oauth2ResponseType
  */
-public class String2ResponseTypeConverter implements Converter<String, ResponseType> {
+public class String2ResponseTypeConverter implements Converter<String, Oauth2ResponseType> {
 
+    /**
+     * Convert String to Oauth2ResponseType, if converting cannot be completed null will be returned
+     * @param source - string source that will be used to converting to Oauth2ResponseType
+     * @return - Oauth2ResponseType by source, null otherwise
+     */
     @Override
-    public ResponseType convert(String source) {
-        return ResponseType.ALL_TYPES.get(source);
+    public Oauth2ResponseType convert(String source) {
+        return OidcResponseType.OIDC_RESPONSE_TYPES.get(source);
     }
 }
