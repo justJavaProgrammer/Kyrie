b3f7bcbca667628cd023261c6e8de1f7408520f1
Odeyalo <theprogrammerinfuture@gmail.com>
2022-12-22 20:05:55 +0200

    Throw it when received wrong authorization code and token can't be obtained

---
+++ src/main/java/com/odeyalo/kyrie/core/oauth2/tokens/InvalidAuthorizationCodeObtainTokenException.java
@@ -0,0 +1,21 @@
+package com.odeyalo.kyrie.core.oauth2.tokens;
+
+import com.odeyalo.kyrie.exceptions.Oauth2ErrorType;
+
+/**
+ * Throw it when received wrong authorization code and token can't be obtained
+ */
+public class InvalidAuthorizationCodeObtainTokenException extends TokenException {
+
+    public InvalidAuthorizationCodeObtainTokenException(String errorDescription) {
+        super(errorDescription, errorDescription, Oauth2ErrorType.INVALID_GRANT);
+    }
+
+    public InvalidAuthorizationCodeObtainTokenException(String message, String errorDescription) {
+        super(message, errorDescription, Oauth2ErrorType.INVALID_GRANT);
+    }
+
+    public InvalidAuthorizationCodeObtainTokenException(String message, Throwable cause, String errorDescription) {
+        super(message, errorDescription, Oauth2ErrorType.INVALID_GRANT, cause);
+    }
+}
