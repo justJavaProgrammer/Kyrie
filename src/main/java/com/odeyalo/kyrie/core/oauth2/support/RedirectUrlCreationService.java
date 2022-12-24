0bdf35232f23a7d703639253b71ed6d27d40cb67
Odeyalo <theprogrammerinfuture@gmail.com>
2022-12-24 22:22:22 +0200

    Create a redirect rul for specific grant type by Oauth2 and Oidc Specifications

---
+++ src/main/java/com/odeyalo/kyrie/core/oauth2/support/RedirectUrlCreationService.java
@@ -0,0 +1,27 @@
+package com.odeyalo.kyrie.core.oauth2.support;
+
+import com.odeyalo.kyrie.core.authorization.AuthorizationGrantType;
+import com.odeyalo.kyrie.core.authorization.AuthorizationRequest;
+import com.odeyalo.kyrie.core.oauth2.Oauth2Token;
+
+/**
+ * Create a redirect rul for specific grant type by Oauth2 and Oidc Specifications.
+ */
+public interface RedirectUrlCreationService {
+
+    /**
+     * Create redirect request by request metadata and token
+     * @param request - AuthorizationRequest with all fields set
+     * @param token - generated token from Oauth2FlowHandler
+     * @return - redirect url required by specific flow
+     * @see com.odeyalo.kyrie.core.oauth2.flow.Oauth2FlowHandler
+     */
+    String createRedirectUrl(AuthorizationRequest request, Oauth2Token token);
+
+
+    /**
+     * Grant type that specific implementation supports
+     * @return - supported grant type
+     */
+    AuthorizationGrantType supportedGrantType();
+}
