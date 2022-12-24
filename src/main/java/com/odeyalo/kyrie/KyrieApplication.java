28e442c4f20f745f9d249ea5763bcdfb081761ba
Odeyalo <theprogrammerinfuture@gmail.com>
2022-12-24 22:28:57 +0200

    Root class

---
+++ src/main/java/com/odeyalo/kyrie/KyrieApplication.java
@@ -0,0 +1,12 @@
+package com.odeyalo.kyrie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyrieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KyrieApplication.class, args);
+    }
+}
