427f7f9e18ff5f55ee0c78f99e18bbae373b7572
Odeyalo <theprogrammerinfuture@gmail.com>
2022-12-24 22:29:09 +0200

    Tests for  KyrieApplication class

---
+++ src/test/java/com/odeyalo/kyrie/KyrieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odeyalo.kyrie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyrieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
