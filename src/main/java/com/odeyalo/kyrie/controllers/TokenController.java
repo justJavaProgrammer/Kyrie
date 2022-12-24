e3af0a7f62e284bfac4c682d4e0e4fcf55cf4700
Odeyalo <theprogrammerinfuture@gmail.com>
2022-12-24 22:24:37 +0200

    Controller to obtain an access token

--- src/main/java/com/odeyalo/kyrie/controllers/TokenController.java
+++ src/main/java/com/odeyalo/kyrie/controllers/TokenController.java
@@ -1,47 +1,76 @@
 package com.odeyalo.kyrie.controllers;
 
 import com.odeyalo.kyrie.controllers.support.AdvancedModelAttribute;
-import com.odeyalo.kyrie.core.oauth2.tokens.AccessTokenReturner;
+import com.odeyalo.kyrie.core.oauth2.Oauth2TokenGeneratorFacade;
+import com.odeyalo.kyrie.core.oauth2.Oauth2ClientCredentials;
+import com.odeyalo.kyrie.core.oauth2.tokens.Oauth2AccessToken;
+import com.odeyalo.kyrie.core.oauth2.tokens.Oauth2AccessTokenManager;
+import com.odeyalo.kyrie.dto.AccessTokenIntrospectionResponse;
 import com.odeyalo.kyrie.dto.GetAccessTokenRequestDTO;
-import com.odeyalo.kyrie.dto.TokensResponse;
+import com.odeyalo.kyrie.dto.KyrieSuccessfulObtainTokenResponse;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
- * Controller to obtain an access token
+ * Controller to obtain an access token and token info
  */
 @RestController
 @Log4j2
 public class TokenController {
-    private final AccessTokenReturner accessTokenReturner;
+    private final Oauth2AccessTokenManager tokenManager;
+    private Oauth2TokenGeneratorFacade generatorFacade;
 
-    public TokenController(AccessTokenReturner accessTokenReturner) {
-        this.accessTokenReturner = accessTokenReturner;
+    public TokenController(Oauth2AccessTokenManager tokenManager, Oauth2TokenGeneratorFacade generatorFacade) {
+        this.tokenManager = tokenManager;
+        this.generatorFacade = generatorFacade;
     }
 
     /**
-     * Method to process /token annotation that support only 'application/json' type
+     * Method to process /token endpoint that support only 'application/json' type
      *
      * @param body - body in json
      * @return - access token
      */
     @PostMapping(value = "/token", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<?> authorization(@RequestBody GetAccessTokenRequestDTO body) {
+    public ResponseEntity<?> resolveAccessTokenUsingJson(@RequestBody GetAccessTokenRequestDTO body) {
         return getStringResponseEntity(body);
     }
 
     @PostMapping(value = "/token", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE, MediaType.APPLICATION_FORM_URLENCODED_VALUE})
-    public ResponseEntity<?> authorizationFormData(@AdvancedModelAttribute GetAccessTokenRequestDTO body) {
+    public ResponseEntity<?> resolveAccessTokenUsingFormData(@AdvancedModelAttribute GetAccessTokenRequestDTO body) {
         return getStringResponseEntity(body);
     }
 
-    private ResponseEntity<TokensResponse> getStringResponseEntity(GetAccessTokenRequestDTO body) {
+    @PostMapping(value = "/tokeninfo", consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public ResponseEntity<?> tokenInfoRfc7662(@RequestParam String token) {
+        log.info("Received token: {}", token);
+        Oauth2AccessToken info = tokenManager.getTokenInfo(token);
+        AccessTokenIntrospectionResponse response = getAccessTokenIntrospectionResponse(info);
+        log.info("Body: {}", response);
+        return ResponseEntity.ok(response);
+    }
+
+    private AccessTokenIntrospectionResponse getAccessTokenIntrospectionResponse(Oauth2AccessToken info) {
+        if (info.isExpired()) {
+            return AccessTokenIntrospectionResponse.nonActive();
+        }
+        return AccessTokenIntrospectionResponse.builder(true)
+                .expiresIn(info.getExpiresIn().getEpochSecond())
+                .scope(info.getScope()).build();
+    }
+
+    private ResponseEntity<KyrieSuccessfulObtainTokenResponse> getStringResponseEntity(GetAccessTokenRequestDTO body) {
         log.info("body {}", body);
         String code = body.getCode();
-        return ResponseEntity.ok(accessTokenReturner.getToken(body.getClientId(), body.getClientSecret(), code));
+        Oauth2AccessToken token = tokenManager.obtainAccessTokenByAuthorizationCode(Oauth2ClientCredentials.of(body.getClientId(), body.getClientSecret()), code);
+        return ResponseEntity.ok(
+                new KyrieSuccessfulObtainTokenResponse(token.getTokenValue(),
+                        token.getTokenType().getValue(),
+                        token.getExpiresIn().getEpochSecond(),
+                        token.getScope())
+        );
     }
 }
+
