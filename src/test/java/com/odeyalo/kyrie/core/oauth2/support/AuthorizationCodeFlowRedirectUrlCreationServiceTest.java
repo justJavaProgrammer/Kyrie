ecffad066b97cca5e6e692185e13f5297d0335ee
Odeyalo <theprogrammerinfuture@gmail.com>
2022-12-25 19:27:59 +0200

    Tests for AuthorizationCodeFlowRedirectUrlCreationService class

---
+++ src/test/java/com/odeyalo/kyrie/core/oauth2/support/AuthorizationCodeFlowRedirectUrlCreationServiceTest.java
@@ -0,0 +1,103 @@
+package com.odeyalo.kyrie.core.oauth2.support;
+
+import com.odeyalo.kyrie.core.Oauth2User;
+import com.odeyalo.kyrie.core.authorization.AuthorizationGrantType;
+import com.odeyalo.kyrie.core.authorization.AuthorizationRequest;
+import com.odeyalo.kyrie.core.authorization.Oauth2ResponseType;
+import com.odeyalo.kyrie.core.oauth2.tokens.code.AuthorizationCode;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Tests for AuthorizationCodeFlowRedirectUrlCreationService class.
+ * @see AuthorizationCodeFlowRedirectUrlCreationService
+ */
+class AuthorizationCodeFlowRedirectUrlCreationServiceTest {
+    private final AuthorizationCodeFlowRedirectUrlCreationService redirectUrlCreationService = new AuthorizationCodeFlowRedirectUrlCreationService();
+
+    @Test
+    @DisplayName("Create redirect url with state param and expect code and state in redirect url")
+    void createRedirectUrlWithStateAndExpectStateAndCode() {
+        String rootRedirectUrl = "http://localhost:6666/callback";
+        String state = "state123";
+        String authCodeValue = "helpmeiwanttokillmyself";
+        String[] scopes = {"read", "write"};
+        AuthorizationRequest request = AuthorizationRequest
+                .builder()
+                .redirectUrl(rootRedirectUrl)
+                .state(state)
+                .grantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .responseTypes(Oauth2ResponseType.CODE)
+                .scopes(scopes)
+                .build();
+        Oauth2User user = new Oauth2User("1", "odeyalo", "password", Set.of("USER"),
+                Collections.emptyMap());
+
+        AuthorizationCode authorizationCode = AuthorizationCode
+                .builder()
+                .codeValue(authCodeValue)
+                .user(user)
+                .expiresIn(LocalDateTime.now()
+                .plusSeconds(60))
+                .scopes(scopes)
+                .build();
+
+        String createdRedirectUrl = redirectUrlCreationService.createRedirectUrl(request, authorizationCode);
+        UriComponents uriComponents = UriComponentsBuilder.fromUriString(createdRedirectUrl).build();
+
+        MultiValueMap<String, String> params = uriComponents.getQueryParams();
+        assertEquals(2, params.size());
+        assertEquals(state, params.getFirst(Oauth2Constants.STATE),
+                "If state parameter was presented in authorization request then redirect url must contain same state as provided in authorization request");
+        assertEquals(authCodeValue, params.getFirst(Oauth2ResponseType.CODE.getSimplifiedName()), "Redirect url must contain code parameter");
+    }
+
+    @Test
+    @DisplayName("Create redirect url withiout state param and expect only code in redirect url")
+    void createRedirectUrlWithoutStateAndExpectOnlyCode() {
+        String rootRedirectUrl = "http://localhost:6666/callback";
+        String state = "state123";
+        String authCodeValue = "helpmeiwanttokillmyself";
+        String[] scopes = {"read", "write"};
+        AuthorizationRequest request = AuthorizationRequest
+                .builder()
+                .redirectUrl(rootRedirectUrl)
+                .grantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .responseTypes(Oauth2ResponseType.CODE)
+                .scopes(scopes)
+                .build();
+        Oauth2User user = new Oauth2User("1", "odeyalo", "password", Set.of("USER"),
+                Collections.emptyMap());
+
+        AuthorizationCode authorizationCode = AuthorizationCode.builder()
+                .codeValue(authCodeValue)
+                .user(user)
+                .expiresIn(LocalDateTime.now().plusSeconds(60))
+                .scopes(scopes)
+                .build();
+
+        String createdRedirectUrl = redirectUrlCreationService.createRedirectUrl(request, authorizationCode);
+        UriComponents uriComponents = UriComponentsBuilder.fromUriString(createdRedirectUrl).build();
+
+        MultiValueMap<String, String> params = uriComponents.getQueryParams();
+        assertEquals(1, params.size());
+        assertNull(params.get(Oauth2Constants.STATE), "If state is not presented then redirect url must contain only an authorization code");
+        assertEquals(authCodeValue, params.getFirst(Oauth2ResponseType.CODE.getSimplifiedName()), "Redirect url must contain an authorization code");
+    }
+
+    @Test
+    void supportedGrantType() {
+        AuthorizationGrantType actual = redirectUrlCreationService.supportedGrantType();
+        AuthorizationGrantType expected = AuthorizationGrantType.AUTHORIZATION_CODE;
+        assertEquals(expected, actual);
+    }
+}
