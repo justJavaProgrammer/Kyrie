21e74669204b2a09a641ed127e50c22e243bcc80
Odeyalo <theprogrammerinfuture@gmail.com>
2022-12-25 22:27:10 +0200

    Added AbstractIntegrationTest and removed SpringBootTest annotation

--- src/test/java/com/odeyalo/kyrie/KyrieApplicationTests.java
+++ src/test/java/com/odeyalo/kyrie/KyrieApplicationTests.java
@@ -3,8 +3,7 @@ package com.odeyalo.kyrie;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class KyrieApplicationTests {
+class KyrieApplicationTests extends AbstractIntegrationTest {
 
     @Test
     void contextLoads() {
