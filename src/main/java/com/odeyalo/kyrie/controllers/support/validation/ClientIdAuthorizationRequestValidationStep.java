ef2e39af7c8454f687f64119164fbaa3bff38d9f
Odeyalo <theprogrammerinfuture@gmail.com>
2022-12-31 17:51:09 +0200

    Implemented logic using Oauth2ClientRepository

--- src/main/java/com/odeyalo/kyrie/controllers/support/validation/ClientIdAuthorizationRequestValidationStep.java
+++ src/main/java/com/odeyalo/kyrie/controllers/support/validation/ClientIdAuthorizationRequestValidationStep.java
@@ -1,6 +1,8 @@
 package com.odeyalo.kyrie.controllers.support.validation;
 
 import com.odeyalo.kyrie.core.authorization.AuthorizationRequest;
+import com.odeyalo.kyrie.core.oauth2.client.Oauth2Client;
+import com.odeyalo.kyrie.core.oauth2.client.Oauth2ClientRepository;
 import com.odeyalo.kyrie.core.support.Oauth2ValidationResult;
 import com.odeyalo.kyrie.exceptions.Oauth2ErrorType;
 import org.springframework.stereotype.Component;
@@ -12,11 +14,18 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class ClientIdAuthorizationRequestValidationStep implements AuthorizationRequestValidationStep {
+    private final Oauth2ClientRepository clientRepository;
 
-    // Todo: Implement the logic
+    public ClientIdAuthorizationRequestValidationStep(Oauth2ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
     @Override
     public Oauth2ValidationResult validate(AuthorizationRequest request) {
         String clientId = request.getClientId();
-        return clientId.equals("client") ? Oauth2ValidationResult.success() : Oauth2ValidationResult.failed(Oauth2ErrorType.INVALID_CLIENT, "The client id does not exist");
+        Oauth2Client client = clientRepository.findOauth2ClientById(clientId);
+        return client != null ?
+                Oauth2ValidationResult.success() :
+                Oauth2ValidationResult.failed(Oauth2ErrorType.INVALID_CLIENT, "The client id does not exist");
     }
 }
