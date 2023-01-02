676e4cfad268bc938edc00f81eec088f22563dd3
Odeyalo <theprogrammerinfuture@gmail.com>
2023-01-02 15:49:09 +0200

    Added conditions

--- src/main/java/com/odeyalo/kyrie/config/KyrieOauth2Configuration.java
+++ src/main/java/com/odeyalo/kyrie/config/KyrieOauth2Configuration.java
@@ -1,27 +1,53 @@
 package com.odeyalo.kyrie.config;
 
+import com.odeyalo.kyrie.core.Oauth2User;
+import com.odeyalo.kyrie.core.authentication.InMemoryOauth2UserAuthenticationService;
+import com.odeyalo.kyrie.core.authentication.Oauth2UserAuthenticationService;
 import com.odeyalo.kyrie.core.oauth2.client.ClientCredentialsValidator;
 import com.odeyalo.kyrie.core.oauth2.client.DefaultClientCredentialsValidator;
 import com.odeyalo.kyrie.core.oauth2.client.Oauth2ClientRepository;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 
-@Configuration
+import javax.annotation.PostConstruct;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * <p>
+ * Configuration class that uses to import the common beans and uses only if
+ * {@link com.odeyalo.kyrie.config.annotation.EnableKyrieAuthorizationServer} annotation is presented on some Spring bean
+ * The configuration beans can be easily  overridden by user's custom configs.
+ * </p>
+ *
+ * @version 1.0
+ */
+@Import({KyrieOauth2ServerEndpointsMappingConfiguration.class})
 public class KyrieOauth2Configuration {
+    private final Logger logger = LoggerFactory.getLogger(KyrieOauth2Configuration.class);
+
+    @PostConstruct
+    void setup() {
+        logger.info("The Authorization Server is enabled, starting Kyrie bootstrap");
+    }
 
     @Bean
-    @ConditionalOnClass(name = "org.springframework.security.web.DefaultSecurityFilterChain")
+    @ConditionalOnMissingBean
     public FilterRegistrationBean<Oauth2ClientValidationFilter> oauth2ClientValidationFilterFilterRegistrationBean(Oauth2ClientValidationFilter oauth2ClientValidationFilter) {
         FilterRegistrationBean<Oauth2ClientValidationFilter> filterRegistration = new FilterRegistrationBean<>();
-        filterRegistration.addUrlPatterns("/oauth2/token");
+        filterRegistration.addUrlPatterns("/token");
         filterRegistration.setFilter(oauth2ClientValidationFilter);
         return filterRegistration;
     }
 
     @Bean
+    @ConditionalOnMissingBean
     public Oauth2ClientValidationFilter oauth2ClientValidationFilter(ClientCredentialsValidator validator) {
         return new Oauth2ClientValidationFilter(validator);
     }
@@ -30,5 +56,18 @@ public class KyrieOauth2Configuration {
     @ConditionalOnMissingBean
     public ClientCredentialsValidator clientCredentialsValidator(Oauth2ClientRepository repository) {
         return new DefaultClientCredentialsValidator(repository);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    @Autowired(required = false)
+    public Oauth2UserAuthenticationService authenticationService(List<Oauth2User> users) {
+        return new InMemoryOauth2UserAuthenticationService(users);
+    }
+
+    @Bean
+    public List<Oauth2User> users() {
+        Oauth2User user = Oauth2User.builder().username("admin").password("123").authorities(Set.of("USER")).additionalInfo(Collections.emptyMap()).build();
+        return List.of(user);
     }
 }
