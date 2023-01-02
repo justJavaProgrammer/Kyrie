361aeda74022c28397c7f5fb6148057b70c3793c
Odeyalo <theprogrammerinfuture@gmail.com>
2023-01-02 15:50:17 +0200

    Configuration to configure validation for Oauth2 requests

---
+++ src/main/java/com/odeyalo/kyrie/config/KyrieOauth2RequestValidationConfiguration.java
@@ -0,0 +1,27 @@
+package com.odeyalo.kyrie.config;
+
+import com.odeyalo.kyrie.controllers.support.AuthorizationRequestValidator;
+import com.odeyalo.kyrie.controllers.support.DefaultChainAuthorizationRequestValidator;
+import com.odeyalo.kyrie.controllers.support.validation.AuthorizationRequestValidationStep;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+public class KyrieOauth2RequestValidationConfiguration {
+
+    /**
+     * Registry AuthorizationRequestValidator only if bean is missing.
+     * @param steps - validation steps to validate an {@link com.odeyalo.kyrie.core.authorization.AuthorizationRequest}
+     * @return - DefaultChainAuthorizationRequestValidator
+     *
+     * @see AuthorizationRequestValidationStep
+     * @see DefaultChainAuthorizationRequestValidator
+     */
+    @Bean
+    @ConditionalOnMissingBean
+    public AuthorizationRequestValidator authorizationRequestValidator(List<AuthorizationRequestValidationStep> steps) {
+        return new DefaultChainAuthorizationRequestValidator(steps);
+    }
+
+}
