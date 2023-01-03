5e6b8e29c61d4f874c42fa888feec70bafffb430
Odeyalo <theprogrammerinfuture@gmail.com>
2023-01-03 16:46:09 +0200

    Added EnableKyrieAuthorizationServer annotation to bootstrap Java-based configs

--- src/test/java/com/odeyalo/kyrie/AbstractIntegrationTest.java
+++ src/test/java/com/odeyalo/kyrie/AbstractIntegrationTest.java
@@ -1,10 +1,12 @@
 package com.odeyalo.kyrie;
 
+import com.odeyalo.kyrie.config.annotation.EnableKyrieAuthorizationServer;
 import org.springframework.boot.test.context.SpringBootTest;
 
 /**
  * Used to cache Spring application context and avoid context recreating to boost performance
  */
 @SpringBootTest
+@EnableKyrieAuthorizationServer
 public class AbstractIntegrationTest {
 }
