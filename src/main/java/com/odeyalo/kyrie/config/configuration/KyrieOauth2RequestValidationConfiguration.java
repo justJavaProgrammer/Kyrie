e972be83365399fdb5f05b03ea2e780b1804a98f
Odeyalo <theprogrammerinfuture@gmail.com>
2023-01-04 14:48:06 +0200

    Added Oauth2ClientValidationFilter bean

--- src/main/java/com/odeyalo/kyrie/config/configuration/KyrieOauth2RequestValidationConfiguration.java
+++ src/main/java/com/odeyalo/kyrie/config/configuration/KyrieOauth2RequestValidationConfiguration.java
@@ -1,10 +1,12 @@
 package com.odeyalo.kyrie.config.configuration;
 
+import com.odeyalo.kyrie.config.Oauth2ClientValidationFilter;
 import com.odeyalo.kyrie.controllers.support.AuthorizationRequestValidator;
 import com.odeyalo.kyrie.controllers.support.DefaultChainAuthorizationRequestValidator;
 import com.odeyalo.kyrie.controllers.support.validation.AuthorizationRequestValidationStep;
 import com.odeyalo.kyrie.controllers.support.validation.ClientIdAuthorizationRequestValidationStep;
 import com.odeyalo.kyrie.controllers.support.validation.RedirectUriAuthorizationRequestValidationStep;
+import com.odeyalo.kyrie.core.oauth2.client.ClientCredentialsValidator;
 import com.odeyalo.kyrie.core.oauth2.client.Oauth2ClientRepository;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +14,13 @@ import org.springframework.context.annotation.Bean;
 import java.util.List;
 
 public class KyrieOauth2RequestValidationConfiguration {
+
+
+    @Bean
+    @ConditionalOnMissingBean
+    public Oauth2ClientValidationFilter oauth2ClientValidationFilter(ClientCredentialsValidator validator, Oauth2ClientRepository oauth2ClientRepository) {
+        return new Oauth2ClientValidationFilter(validator, oauth2ClientRepository);
+    }
 
     /**
      * Registry AuthorizationRequestValidator only if bean is missing.
