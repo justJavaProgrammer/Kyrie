1aa7a45bb1e4495931fb8c3a0ca368849bf824b2
Odeyalo <theprogrammerinfuture@gmail.com>
2023-01-07 20:34:31 +0200

    The interface is used to enhance the Model that will be used to render view

---
+++ src/main/java/com/odeyalo/kyrie/support/html/ModelEnhancerPostProcessor.java
@@ -0,0 +1,17 @@
+package com.odeyalo.kyrie.support.html;
+
+import org.springframework.ui.Model;
+
+/**
+ * The interface is used to enhance the Model that will be used to render view.
+ */
+public interface ModelEnhancerPostProcessor {
+
+    /**
+     * Enhance the Model with parameters
+     * @param templateType - type of the template that will be returned as response
+     * @param model - model to enhance
+     */
+    void enhanceModel(String templateType, Model model);
+
+}
