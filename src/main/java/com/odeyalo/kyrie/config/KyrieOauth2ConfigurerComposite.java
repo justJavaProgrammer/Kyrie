4d7923e656b23ef301d6d11cfc86272baa1d59e3
Odeyalo <theprogrammerinfuture@gmail.com>
2023-01-07 20:30:34 +0200

    KyrieOauth2Configurer implementation that just delegates job to others  KyrieOauth2Configurer that was previously registered in.

---
+++ src/main/java/com/odeyalo/kyrie/config/KyrieOauth2ConfigurerComposite.java
@@ -0,0 +1,36 @@
+package com.odeyalo.kyrie.config;
+
+import com.odeyalo.kyrie.config.configurers.Oauth2ServerEndpointsConfigurer;
+import org.springframework.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Simple {@link KyrieOauth2Configurer} implementation   that just delegate configuration to other KyrieOauth2Configurer(s)
+ *
+ * @version 1.0
+ * @see KyrieOauth2Configurer
+ */
+public class KyrieOauth2ConfigurerComposite implements KyrieOauth2Configurer {
+    private final List<KyrieOauth2Configurer> delegates = new ArrayList<>();
+
+    /**
+     * Registry all configurers if list is not null or empty
+     *
+     * @param configurers - configurers that will be used as delegates
+     */
+    public void addAll(List<KyrieOauth2Configurer> configurers) {
+        if (CollectionUtils.isEmpty(configurers)) {
+            return;
+        }
+        delegates.addAll(configurers);
+    }
+
+    @Override
+    public void configureEndpoints(Oauth2ServerEndpointsConfigurer configurer) {
+        for (KyrieOauth2Configurer delegate : delegates) {
+            delegate.configureEndpoints(configurer);
+        }
+    }
+}
