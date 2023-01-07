68479767116e3fbb1c50da648e63055b2b1e3404
Odeyalo <theprogrammerinfuture@gmail.com>
2023-01-07 20:35:48 +0200

    Logic to resolve Oauth2Client moded to Oauth2ClientCredentialsResolverHelper

--- src/main/java/com/odeyalo/kyrie/config/Oauth2ClientValidationFilter.java
+++ src/main/java/com/odeyalo/kyrie/config/Oauth2ClientValidationFilter.java
@@ -5,7 +5,6 @@ import com.odeyalo.kyrie.core.oauth2.client.ClientCredentialsValidator;
 import com.odeyalo.kyrie.core.oauth2.client.Oauth2Client;
 import com.odeyalo.kyrie.core.oauth2.client.Oauth2ClientRepository;
 import com.odeyalo.kyrie.core.support.ValidationResult;
-import org.springframework.http.HttpHeaders;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -15,7 +14,6 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Base64;
 
 /**
  * <p>Filter to verify the Oauth2 Client credentials.
@@ -38,24 +36,20 @@ import java.util.Base64;
  * @version 1.0
  */
 public class Oauth2ClientValidationFilter extends OncePerRequestFilter {
-    public static final String BASIC_AUTHENTICATION_PREFIX = "Basic ";
     private final ClientCredentialsValidator clientCredentialsValidator;
     private final Oauth2ClientRepository oauth2ClientRepository;
+    private final Oauth2ClientCredentialsResolverHelper clientCredentialsResolverHelper;
 
-    public Oauth2ClientValidationFilter(ClientCredentialsValidator clientCredentialsValidator, Oauth2ClientRepository oauth2ClientRepository) {
+    public Oauth2ClientValidationFilter(ClientCredentialsValidator clientCredentialsValidator, Oauth2ClientRepository oauth2ClientRepository, Oauth2ClientCredentialsResolverHelper clientCredentialsResolverHelper) {
         this.clientCredentialsValidator = clientCredentialsValidator;
         this.oauth2ClientRepository = oauth2ClientRepository;
+        this.clientCredentialsResolverHelper = clientCredentialsResolverHelper;
     }
 
     @Override
     public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws IOException, ServletException {
-        String authHeader = request.getHeader(HttpHeaders.AUTHORIZATION);
-        Oauth2ClientCredentials clientCredentials;
-        if (authHeader == null) {
-            clientCredentials = getOauth2ClientCredentialsByParameters(request);
-        } else {
-            clientCredentials = parseBasicAuthentication(authHeader);
-        }
+        Oauth2ClientCredentials clientCredentials = clientCredentialsResolverHelper.resolveCredentials(request, true);
+
         // If credentials are null then filter check is failed and other checks are useless
         if (clientCredentials == null) {
             filterChain.doFilter(request, response);
@@ -70,40 +64,5 @@ public class Oauth2ClientValidationFilter extends OncePerRequestFilter {
         }
 
         filterChain.doFilter(request, response);
-    }
-
-    /**
-     * Using to resolve Oauth2ClientCredentials by request parameter that was provided in request.
-     * @param request - request with parameters
-     * @return - Oauth2ClientCredentials that was resolved from request parameters
-     * or null if request does not contain client_id or client_secret parameters
-     */
-    private Oauth2ClientCredentials getOauth2ClientCredentialsByParameters(HttpServletRequest request) {
-        String clientId = request.getParameter("client_id");
-        String clientSecret = request.getParameter("client_secret");
-        if (clientId == null || clientSecret == null) {
-            return null;
-        }
-        return Oauth2ClientCredentials.of(clientId, clientSecret);
-    }
-
-    /**
-     * Using to resolve Oauth2ClientCredentials by 'Authorization' HTTP Header using Basic Authentication
-     * @param authHeader - 'Authorization' header from request
-     * @return - resolved {@link Oauth2ClientCredentials} or null if Authorization header contain not Basic-Authentication type of authentication.
-     */
-    private Oauth2ClientCredentials parseBasicAuthentication(String authHeader) {
-        if (!authHeader.startsWith(BASIC_AUTHENTICATION_PREFIX)) {
-            return null;
-        }
-        String basicHeaderValue = authHeader.substring(BASIC_AUTHENTICATION_PREFIX.length());
-        String decodedCredentials = new String(Base64.getDecoder().decode(basicHeaderValue));
-        String[] usernamePassword = decodedCredentials.split(":");
-        if (usernamePassword.length != 2) {
-            return null;
-        }
-        String clientId = usernamePassword[0];
-        String clientSecret = usernamePassword[1];
-        return Oauth2ClientCredentials.of(clientId, clientSecret);
     }
 }
