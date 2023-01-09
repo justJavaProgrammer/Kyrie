56c4d53e18b92ce86250f810cba23c921eb58d03
Odeyalo <theprogrammerinfuture@gmail.com>
2023-01-09 17:32:51 +0200

    Simple class that used to store the AuthorizationRequest

---
+++ src/main/java/com/odeyalo/kyrie/core/support/AuthorizationRequestContext.java
@@ -0,0 +1,34 @@
+package com.odeyalo.kyrie.core.support;
+
+import com.odeyalo.kyrie.core.authorization.AuthorizationRequest;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+/**
+ * Simple class that used to store the {@link AuthorizationRequest}
+ *
+ * @see AuthorizationRequest
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthorizationRequestContext {
+    private AuthorizationRequest request;
+
+    /**
+     * Returns the {@link AuthorizationRequest}, null if request was not set
+     *
+     * @return - AuthorizationRequest, null otherwise
+     */
+    public AuthorizationRequest getRequest() {
+        return request;
+    }
+
+    /**
+     * Set the {@link AuthorizationRequest} in context
+     *
+     * @param request - request to set
+     */
+    public void setRequest(AuthorizationRequest request) {
+        this.request = request;
+    }
+}
