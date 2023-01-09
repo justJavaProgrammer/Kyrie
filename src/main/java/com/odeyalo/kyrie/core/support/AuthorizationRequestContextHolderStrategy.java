dda857dd8e991b787383d52544bdeb06f6691c80
Odeyalo <theprogrammerinfuture@gmail.com>
2023-01-09 17:34:59 +0200

    A strategy for storing authorization request information against a thread.

---
+++ src/main/java/com/odeyalo/kyrie/core/support/AuthorizationRequestContextHolderStrategy.java
@@ -0,0 +1,29 @@
+package com.odeyalo.kyrie.core.support;
+
+/**
+ *  A strategy for storing authorization request information against a thread.
+ *
+ *  <p>The preferred strategy is loaded by {@link AuthorizationRequestContextHolder}</p>
+ */
+public interface AuthorizationRequestContextHolderStrategy {
+
+    /**
+     * Clears the context for the given thread
+     */
+    void clearContext();
+
+    /**
+     * Set the current context.
+     * @param context - context to set
+     */
+    void setContext(AuthorizationRequestContext context);
+
+    /**
+     * Obtains the current context.
+     * @return - AuthorizationRequestContext that was previously set, null otherwise
+     */
+    AuthorizationRequestContext getContext();
+
+
+    AuthorizationRequestContext createEmptyContext();
+}
