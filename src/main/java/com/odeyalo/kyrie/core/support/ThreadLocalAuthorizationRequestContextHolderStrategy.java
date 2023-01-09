56a0708de36eb1638fd30f1b9a99ceb484936ad0
Odeyalo <theprogrammerinfuture@gmail.com>
2023-01-09 17:35:33 +0200

    Implementation that uses ThreadLocal to store the AuthorizationRequest

---
+++ src/main/java/com/odeyalo/kyrie/core/support/ThreadLocalAuthorizationRequestContextHolderStrategy.java
@@ -0,0 +1,39 @@
+package com.odeyalo.kyrie.core.support;
+
+import org.springframework.util.Assert;
+
+/**
+ * {@link AuthorizationRequestContextHolderStrategy} implementation that uses ThreadLocal to store the {@link AuthorizationRequestContext}.
+ *
+ *
+ * @see AuthorizationRequestContextHolderStrategy
+ * @see AuthorizationRequestContext
+ * @see ThreadLocal
+ */
+public class ThreadLocalAuthorizationRequestContextHolderStrategy implements AuthorizationRequestContextHolderStrategy {
+    private static final ThreadLocal<AuthorizationRequestContext> holder = new ThreadLocal<>();
+
+    @Override
+    public void clearContext() {
+        holder.remove();
+    }
+
+    @Override
+    public void setContext(AuthorizationRequestContext context) {
+        Assert.notNull(context, "The AuthorizationRequestContext must be not null");
+        System.out.println("Set " + Thread.currentThread().getName());
+        holder.set(context);
+    }
+
+    @Override
+    public AuthorizationRequestContext getContext() {
+        AuthorizationRequestContext context = holder.get();
+        System.out.println("Get " + Thread.currentThread().getName());
+        return context != null ? context : createEmptyContext();
+    }
+
+    @Override
+    public AuthorizationRequestContext createEmptyContext() {
+        return new AuthorizationRequestContext();
+    }
+}
