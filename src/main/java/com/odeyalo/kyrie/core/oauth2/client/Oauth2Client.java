6d26b4a261df9bfba90d7c7b5aecc257b6e2357f
Odeyalo <theprogrammerinfuture@gmail.com>
2023-01-10 20:25:20 +0200

    Added ClientType

--- src/main/java/com/odeyalo/kyrie/core/oauth2/client/Oauth2Client.java
+++ src/main/java/com/odeyalo/kyrie/core/oauth2/client/Oauth2Client.java
@@ -32,6 +32,7 @@ public class Oauth2Client implements UserDetails {
      */
     @Singular("allowedRedirectUri")
     private final Set<String> allowedRedirectUris;
+    private final ClientType clientType;
 
     public void addAllowedRedirectUri(String redirectUri) {
         this.allowedRedirectUris.add(redirectUri);
@@ -70,5 +71,16 @@ public class Oauth2Client implements UserDetails {
     @Override
     public boolean isEnabled() {
         return true;
+    }
+
+    /**
+     * Type of the client.
+     *
+     * Confidential clients are applications that are able to securely authenticate with the authorization server, for example being able to keep their registered client secret safe.
+     * Public clients are unable to use registered client secrets, such as applications running in a browser or on a mobile device.
+     */
+    public enum ClientType {
+        PUBLIC,
+        CONFIDENTIAL
     }
 }
