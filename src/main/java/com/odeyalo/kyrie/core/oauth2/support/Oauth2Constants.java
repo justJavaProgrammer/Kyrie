092bfff1c8b9cc1d499d107167099837ccb71ca6
Odeyalo <theprogrammerinfuture@gmail.com>
2023-01-12 14:58:13 +0200

    Added additional constants

--- src/main/java/com/odeyalo/kyrie/core/oauth2/support/Oauth2Constants.java
+++ src/main/java/com/odeyalo/kyrie/core/oauth2/support/Oauth2Constants.java
@@ -12,4 +12,10 @@ public class Oauth2Constants {
     public static final String STATE = "state";
     public static final String TOKEN_TYPE = "token_type";
     public static final String EXPIRES_IN = "expires_in";
+    public static final String SCOPE = "scope";
+    public static final String GRANT_TYPE = "grant_type";
+    public static final String CLIENT_ID = "client_id";
+    public static final String CLIENT_SECRET = "client_secret";
+    public static final String ERROR_PARAMETER_NAME = "error";
+    public static final String ERROR_DESCRIPTION_PARAMETER_NAME = "error_description";
 }
