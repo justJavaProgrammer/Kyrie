24dbe3f272c90ed9ae714fd4dc2414a9eeb4d535
Odeyalo <theprogrammerinfuture@gmail.com>
2023-01-13 20:13:39 +0200

    Added EnableKyrieAuthorizationServer annotation

--- src/main/java/com/odeyalo/kyrie/KyrieApplication.java
+++ src/main/java/com/odeyalo/kyrie/KyrieApplication.java
@@ -1,9 +1,11 @@
 package com.odeyalo.kyrie;
 
+import com.odeyalo.kyrie.config.annotation.EnableKyrieAuthorizationServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableKyrieAuthorizationServer
 public class KyrieApplication {
 
     public static void main(String[] args) {
