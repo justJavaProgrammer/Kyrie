52fbe3e8d232bf9584c84ad0e7e2da55a2ff88bb
Odeyalo <theprogrammerinfuture@gmail.com>
2023-01-13 20:12:48 +0200

    Added tests

--- src/test/java/com/odeyalo/kyrie/config/DefaultCompositeOauth2ClientCredentialsResolverTest.java
+++ src/test/java/com/odeyalo/kyrie/config/DefaultCompositeOauth2ClientCredentialsResolverTest.java
@@ -1,5 +1,8 @@
 package com.odeyalo.kyrie.config;
 
+import com.odeyalo.kyrie.config.support.BasicAuthenticationOauth2ClientCredentialsResolverHelper;
+import com.odeyalo.kyrie.config.support.RequestBodyOauth2ClientCredentialsResolverHelper;
+import com.odeyalo.kyrie.config.support.RequestParametersOauth2ClientCredentialsResolverHelper;
 import com.odeyalo.kyrie.core.oauth2.Oauth2ClientCredentials;
 import org.junit.jupiter.api.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
@@ -9,8 +12,15 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+//todo: Write tests
 class DefaultCompositeOauth2ClientCredentialsResolverTest {
-    DefaultCompositeOauth2ClientCredentialsResolver helper = new DefaultCompositeOauth2ClientCredentialsResolver(List.of());
+    DefaultCompositeOauth2ClientCredentialsResolver helper = new DefaultCompositeOauth2ClientCredentialsResolver(
+            List.of(
+                    new BasicAuthenticationOauth2ClientCredentialsResolverHelper(),
+                    new RequestBodyOauth2ClientCredentialsResolverHelper(),
+                    new RequestParametersOauth2ClientCredentialsResolverHelper()
+            )
+    );
 
     @Test
     void resolveCredentials() {
@@ -32,5 +42,12 @@ class DefaultCompositeOauth2ClientCredentialsResolverTest {
         Oauth2ClientCredentials clientCredentials = helper.resolveCredentials(request, false);
         assertEquals(clientId, clientCredentials.getClientId());
         assertNull(clientCredentials.getClientSecret());
+    }
+
+    @Test
+    void resolveCredentialsWithNull() {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        Oauth2ClientCredentials clientCredentials = helper.resolveCredentials(request, false);
+        assertNull(clientCredentials);
     }
 }
