fdf3ce1ac74b8a225ab816c762c908c201ffed6b
Odeyalo <theprogrammerinfuture@gmail.com>
2023-01-20 19:04:30 +0200

    Event that will be occurred when theOauth2Client attempted to login

---
+++ src/main/java/com/odeyalo/kyrie/core/events/Oauth2ClientLoginAuthenticationAttemptedKyrieEvent.java
@@ -0,0 +1,17 @@
+package com.odeyalo.kyrie.core.events;
+
+import com.odeyalo.kyrie.core.events.authentication.AbstractAuthenticationAttemptedKyrieEvent;
+
+/**
+ * Event that will be occurred when the {@link com.odeyalo.kyrie.core.oauth2.client.Oauth2Client} attempted to login
+ */
+public class Oauth2ClientLoginAuthenticationAttemptedKyrieEvent extends AbstractAuthenticationAttemptedKyrieEvent {
+
+    public Oauth2ClientLoginAuthenticationAttemptedKyrieEvent(AttemptedLoginAuthentication authentication) {
+        super(authentication);
+    }
+
+    public Oauth2ClientLoginAuthenticationAttemptedKyrieEvent(AttemptedLoginAuthentication authentication, String eventId) {
+        super(authentication, eventId);
+    }
+}
