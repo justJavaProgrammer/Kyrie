ad2be051471774c9005813a7cacefffa8b98d510
Odeyalo <theprogrammerinfuture@gmail.com>
2023-01-20 19:02:56 +0200

    Event that will be occurred when user login is failed and reason is bad credentials provided by user

---
+++ src/main/java/com/odeyalo/kyrie/core/events/authentication/UserLoginAuthenticationFailureBadCredentialsKyrieEvent.java
@@ -0,0 +1,15 @@
+package com.odeyalo.kyrie.core.events.authentication;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+/**
+ * Event that will be occurred when user login is failed and reason is bad credentials provided by user.
+ */
+public class UserLoginAuthenticationFailureBadCredentialsKyrieEvent extends AbstractAuthenticationFailureKyrieEvent {
+
+
+    public UserLoginAuthenticationFailureBadCredentialsKyrieEvent(Authentication authentication, AuthenticationException authenticationException) {
+        super(authentication, authenticationException);
+    }
+}
