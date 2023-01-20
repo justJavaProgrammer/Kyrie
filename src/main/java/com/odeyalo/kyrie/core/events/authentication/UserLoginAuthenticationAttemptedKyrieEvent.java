29908c4f45134a6ecacec6cc29a32c409e5b965a
Odeyalo <theprogrammerinfuture@gmail.com>
2023-01-20 19:05:08 +0200

    The event will be occurred every time when the user tried to login

---
+++ src/main/java/com/odeyalo/kyrie/core/events/authentication/UserLoginAuthenticationAttemptedKyrieEvent.java
@@ -0,0 +1,23 @@
+package com.odeyalo.kyrie.core.events.authentication;
+
+import org.apache.commons.lang.RandomStringUtils;
+
+/**
+ * The event will be occurred every time when the user tried to login
+ */
+public class UserLoginAuthenticationAttemptedKyrieEvent extends AbstractAuthenticationAttemptedKyrieEvent {
+    /**
+     * Create a new UserLoginAuthenticationAttemptedKyrieEvent
+     * @param authentication- {@link AttemptedLoginAuthentication} with required fields set
+     */
+    public UserLoginAuthenticationAttemptedKyrieEvent(AttemptedLoginAuthentication authentication) {
+        super(authentication, RandomStringUtils.randomAlphanumeric(20));
+    }
+    /**
+     * Create a new UserLoginAuthenticationAttemptedKyrieEvent with custom event id
+     * @param eventId - custom event id
+     */
+    public UserLoginAuthenticationAttemptedKyrieEvent(AttemptedLoginAuthentication authentication, String eventId) {
+        super(authentication, eventId);
+    }
+}
