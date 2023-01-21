702c87eeb738fb26910b56d2bb635884744a635a
Odeyalo <theprogrammerinfuture@gmail.com>
2023-01-21 17:37:45 +0200

    Interface can be used to listen to the Oauth2ClientLoginAuthenticationGrantedKyrieEvent

---
+++ src/main/java/com/odeyalo/kyrie/core/events/listener/Oauth2ClientLoginAuthenticationGrantedKyrieEventListener.java
@@ -0,0 +1,9 @@
+package com.odeyalo.kyrie.core.events.listener;
+
+import com.odeyalo.kyrie.core.events.authentication.Oauth2ClientLoginAuthenticationGrantedKyrieEvent;
+
+/**
+ * Interface that can be used to listen to the {@link Oauth2ClientLoginAuthenticationGrantedKyrieEvent}
+ */
+public interface Oauth2ClientLoginAuthenticationGrantedKyrieEventListener extends KyrieEventListener<Oauth2ClientLoginAuthenticationGrantedKyrieEvent>{
+}
