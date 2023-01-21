570a52f638874cbeb10fb580fea222ddff733a34
Odeyalo <theprogrammerinfuture@gmail.com>
2023-01-21 17:37:17 +0200

    Interface can be used to listen to the Oauth2ClientAuthenticationFailureBadCredentialsKyrieEvent

---
+++ src/main/java/com/odeyalo/kyrie/core/events/listener/Oauth2ClientAuthenticationFailureBadCredentialsKyrieEventListener.java
@@ -0,0 +1,9 @@
+package com.odeyalo.kyrie.core.events.listener;
+
+import com.odeyalo.kyrie.core.events.authentication.Oauth2ClientAuthenticationFailureBadCredentialsKyrieEvent;
+
+/**
+ * Interface can be used to listen to the {@link Oauth2ClientAuthenticationFailureBadCredentialsKyrieEvent}
+ */
+public interface Oauth2ClientAuthenticationFailureBadCredentialsKyrieEventListener extends KyrieEventListener<Oauth2ClientAuthenticationFailureBadCredentialsKyrieEvent> {
+}
