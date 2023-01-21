836c9d64387e2c663c94d5adfefe2d5b0b416b7b
Odeyalo <theprogrammerinfuture@gmail.com>
2023-01-21 17:33:05 +0200

    Interface that can be used to listen to the UserLoginAuthenticationAttemptedKyrieEvent

---
+++ src/main/java/com/odeyalo/kyrie/core/events/listener/UserLoginAuthenticationAttemptedKyrieEventListener.java
@@ -0,0 +1,9 @@
+package com.odeyalo.kyrie.core.events.listener;
+
+import com.odeyalo.kyrie.core.events.authentication.UserLoginAuthenticationAttemptedKyrieEvent;
+
+/**
+ * Interface that can be used to listen to the {@link UserLoginAuthenticationAttemptedKyrieEvent}
+ */
+public interface UserLoginAuthenticationAttemptedKyrieEventListener extends KyrieEventListener<UserLoginAuthenticationAttemptedKyrieEvent> {
+}
