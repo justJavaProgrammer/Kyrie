d049956609624908580b84d1ecda2dff6e1a4c9c
Odeyalo <theprogrammerinfuture@gmail.com>
2023-01-25 23:03:23 +0200

    ImplicitFlowRedirectUrlCreationService implementation to customize redirect uri

---
+++ src/main/java/com/odeyalo/kyrie/core/oauth2/support/decorator/CustomizableImplicitFlowRedirectUrlCreationServiceDecorator.java
@@ -0,0 +1,21 @@
+package com.odeyalo.kyrie.core.oauth2.support.decorator;
+
+import com.odeyalo.kyrie.core.oauth2.support.ImplicitFlowRedirectUrlCreationService;
+import com.odeyalo.kyrie.core.oauth2.support.RedirectUrlCreationService;
+import com.odeyalo.kyrie.core.oauth2.tokens.customizer.Oauth2TokenCustomizerProcessorRegistry;
+
+/**
+ * Simple {@link ImplicitFlowRedirectUrlCreationService} that just delegates all job to {@link AbstractCustomizableRedirectUriCreationServiceDecorator}
+ */
+public class CustomizableImplicitFlowRedirectUrlCreationServiceDecorator extends AbstractCustomizableRedirectUriCreationServiceDecorator implements ImplicitFlowRedirectUrlCreationService {
+
+    /**
+     * Create new CustomizableImplicitFlowRedirectUrlCreationServiceDecorator with delegate and customizers
+     *
+     * @param delegate    - parent {@link RedirectUrlCreationService} to delegate and create redirect uri that will be used to customize
+     * @param customizers - customizers holder
+     */
+    protected CustomizableImplicitFlowRedirectUrlCreationServiceDecorator(ImplicitFlowRedirectUrlCreationService delegate, Oauth2TokenCustomizerProcessorRegistry customizers) {
+        super(delegate, customizers);
+    }
+}
