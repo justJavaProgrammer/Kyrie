746605fc37d613b0ec03331c5d2cef08e689925d
Odeyalo <theprogrammerinfuture@gmail.com>
2023-01-25 17:03:57 +0200

    Added REFRESH_TOKEN

--- src/main/java/com/odeyalo/kyrie/core/oauth2/support/Oauth2Constants.java
+++ src/main/java/com/odeyalo/kyrie/core/oauth2/support/Oauth2Constants.java
@@ -20,4 +20,5 @@ public class Oauth2Constants {
     public static final String ERROR_DESCRIPTION_PARAMETER_NAME = "error_description";
     public static final String RESPONSE_TYPE = "response_type";
     public static final String REDIRECT_URI = "redirect_uri";
+    public static final String REFRESH_TOKEN = "refresh_token";
 }
