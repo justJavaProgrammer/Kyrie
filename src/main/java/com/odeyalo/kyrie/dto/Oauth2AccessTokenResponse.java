605c4a8b925b56cab64c65e253aa76dac060e1c3
Odeyalo <theprogrammerinfuture@gmail.com>
2023-01-26 18:04:53 +0200

    Added support to return additional info

--- src/main/java/com/odeyalo/kyrie/dto/Oauth2AccessTokenResponse.java
+++ src/main/java/com/odeyalo/kyrie/dto/Oauth2AccessTokenResponse.java
@@ -1,33 +1,50 @@
 package com.odeyalo.kyrie.dto;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.odeyalo.kyrie.core.oauth2.tokens.TokenMetadata;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.odeyalo.kyrie.core.oauth2.support.Oauth2Constants;
 import com.odeyalo.kyrie.core.oauth2.tokens.Oauth2AccessToken;
+import com.odeyalo.kyrie.core.oauth2.tokens.TokenMetadata;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.Singular;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Represent access token response for /token endpoint
  */
 @AllArgsConstructor
 @Data
+@Builder
 public class Oauth2AccessTokenResponse {
     private final boolean active;
     @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty(Oauth2Constants.ACCESS_TOKEN)
     private final String token;
     @JsonInclude(JsonInclude.Include.NON_NULL)
-    private final String prefix;
+    @JsonProperty("token_type")
+    private final String tokenType;
     @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty("expires_in")
     private final Long expiresIn;
+    @JsonProperty(Oauth2Constants.SCOPE)
     @JsonInclude(JsonInclude.Include.NON_NULL)
     private final String scopes;
+    @Singular
+    private final Map<String, Object> additionalParameters;
 
     private Oauth2AccessTokenResponse(boolean active) {
         this.active = active;
         this.token = null;
-        this.prefix = null;
+        this.tokenType = null;
         this.expiresIn = null;
         this.scopes = null;
+        this.additionalParameters = new HashMap<>();
     }
 
     /**
@@ -45,7 +62,13 @@ public class Oauth2AccessTokenResponse {
                 token.getTokenValue(),
                 token.getTokenType().getValue(),
                 token.getExpiresIn().getEpochSecond(),
-                token.getScope()
+                token.getScope(),
+                Collections.emptyMap()
         );
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getAdditionalParameters() {
+        return additionalParameters;
     }
 }
