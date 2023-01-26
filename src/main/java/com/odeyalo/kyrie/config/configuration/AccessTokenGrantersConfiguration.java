cc1e2711e45cfbb603e8de22792fe82b158b309f
Odeyalo <theprogrammerinfuture@gmail.com>
2023-01-26 18:10:19 +0200

    Added AccessTokenGranterStrategyFacadeWrapper bean

--- src/main/java/com/odeyalo/kyrie/config/configuration/AccessTokenGrantersConfiguration.java
+++ src/main/java/com/odeyalo/kyrie/config/configuration/AccessTokenGrantersConfiguration.java
@@ -3,9 +3,13 @@ package com.odeyalo.kyrie.config.configuration;
 import com.odeyalo.kyrie.core.authentication.EventPublisherOauth2UserAuthenticationServiceDecorator;
 import com.odeyalo.kyrie.core.authentication.Oauth2UserAuthenticationService;
 import com.odeyalo.kyrie.core.events.KyrieEventPublisher;
+import com.odeyalo.kyrie.core.oauth2.tokens.AccessTokenGranterStrategyFactory;
 import com.odeyalo.kyrie.core.oauth2.tokens.AuthorizationCodeExchangeAccessTokenGranterStrategy;
 import com.odeyalo.kyrie.core.oauth2.tokens.AuthorizationCodeFlowAccessTokenReturner;
 import com.odeyalo.kyrie.core.oauth2.tokens.PasswordFlowAccessTokenGranterStrategy;
+import com.odeyalo.kyrie.core.oauth2.tokens.customizer.Oauth2TokenCustomizerProcessorRegistry;
+import com.odeyalo.kyrie.core.oauth2.tokens.facade.AccessTokenGranterStrategyFacadeWrapper;
+import com.odeyalo.kyrie.core.oauth2.tokens.facade.SimpleAccessTokenGranterStrategyFacadeWrapper;
 import com.odeyalo.kyrie.core.oauth2.tokens.jwt.Oauth2AccessTokenGenerator;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
@@ -16,6 +20,11 @@ import org.springframework.context.annotation.Primary;
 @AutoConfigureAfter(GenericKyrieOauth2Configuration.class)
 public class AccessTokenGrantersConfiguration {
 
+    @Bean
+    public AccessTokenGranterStrategyFacadeWrapper accessTokenGranterStrategyFacadeWrapper(AccessTokenGranterStrategyFactory factory, Oauth2TokenCustomizerProcessorRegistry registry) {
+        return new SimpleAccessTokenGranterStrategyFacadeWrapper(factory, registry);
+    }
+//todo rewrite this bean with BPP
     @Bean
     @Primary
     @DependsOn("oauth2UserAuthenticationService")
