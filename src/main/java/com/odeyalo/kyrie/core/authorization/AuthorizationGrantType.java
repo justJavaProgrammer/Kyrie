548caa4fab072f570260ef29b4d9ee1b9cd5120c
Odeyalo <theprogrammerinfuture@gmail.com>
2023-01-28 17:20:59 +0200

    Rewritten enum to class

--- src/main/java/com/odeyalo/kyrie/core/authorization/AuthorizationGrantType.java
+++ src/main/java/com/odeyalo/kyrie/core/authorization/AuthorizationGrantType.java
@@ -2,51 +2,49 @@ package com.odeyalo.kyrie.core.authorization;
 
 import com.odeyalo.kyrie.core.oauth2.oidc.OidcResponseType;
 
-import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 /**
  * Enum to represent grant type for Oauth2.
  * @see <a href="https://www.rfc-editor.org/rfc/rfc6749#section-1.3">Grant Type</a>
- * @version 1.0
+ * @version 2.0
  */
-public enum AuthorizationGrantType {
+public class AuthorizationGrantType {
     /**
      * Authorization code grant type.
      * Refer to <a href="https://www.rfc-editor.org/rfc/rfc6749#section-1.3.1">1.3.1.  Authorization Code</a>
      */
-    AUTHORIZATION_CODE("authorization_code", Oauth2ResponseType.CODE),
+    public static final AuthorizationGrantType AUTHORIZATION_CODE = new AuthorizationGrantType("authorization_code", Oauth2ResponseType.CODE);
     /**
      * Implicit grant type
      * Refer to <a href="https://www.rfc-editor.org/rfc/rfc6749#section-1.3.2">Implicit flow</a>
      */
-    IMPLICIT("implicit", Oauth2ResponseType.TOKEN),
+    public static final AuthorizationGrantType IMPLICIT = new AuthorizationGrantType("implicit", Oauth2ResponseType.TOKEN);
 
     /**
      * Represent custom multiple response type. Same as hybrid but supports Implicit flow too.
      */
-    MULTIPLE("multiple", Oauth2ResponseType.CODE, Oauth2ResponseType.TOKEN, OidcResponseType.ID_TOKEN),
+    public static final AuthorizationGrantType MULTIPLE = new AuthorizationGrantType("multiple", Oauth2ResponseType.CODE, Oauth2ResponseType.TOKEN, OidcResponseType.ID_TOKEN);
 
     /**
      * Represent Password grant type. Refer to <a href="https://www.rfc-editor.org/rfc/rfc6749#section-4.3">Password grant</a>
      */
-    PASSWORD("password", Oauth2ResponseType.TOKEN);
+    public static final AuthorizationGrantType PASSWORD = new AuthorizationGrantType("password", Oauth2ResponseType.TOKEN);
 
     private final String grantName;
     private final Oauth2ResponseType[] supportedResponseType;
 
-    public static final Map<String,AuthorizationGrantType> ALL_TYPES =
-            Map.copyOf(Arrays.stream(values())
-                    .collect(Collectors.toMap(authorizationGrantType -> authorizationGrantType.grantName, st -> st)));
+    public static Map<String,AuthorizationGrantType> types;
 
-    AuthorizationGrantType(String grantName, Oauth2ResponseType... supportedResponseType) {
+    public AuthorizationGrantType(String grantName, Oauth2ResponseType... supportedResponseType) {
         this.grantName = grantName;
         this.supportedResponseType = supportedResponseType;
+        addType(grantName, this);
     }
 
     public static AuthorizationGrantType fromSimplifiedName(String name) {
-        return ALL_TYPES.get(name);
+        return types.get(name);
     }
 
     public String getGrantName() {
@@ -55,5 +53,12 @@ public enum AuthorizationGrantType {
 
     public Oauth2ResponseType[] getSupportedResponseTypes() {
         return supportedResponseType;
+    }
+
+    public static void addType(String grantName, AuthorizationGrantType grantType) {
+        if (types == null) {
+            types = new LinkedHashMap<>();
+        }
+        types.put(grantName, grantType);
     }
 }
