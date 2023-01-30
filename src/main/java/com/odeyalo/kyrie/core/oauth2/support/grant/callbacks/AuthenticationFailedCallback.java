a75c5b66415323cc88527025cb45bc4fefdab9d8
Odeyalo <theprogrammerinfuture@gmail.com>
2023-01-30 21:26:51 +0200

    Callback that will be invoked when the user authentication is failed

---
+++ src/main/java/com/odeyalo/kyrie/core/oauth2/support/grant/callbacks/AuthenticationFailedCallback.java
@@ -0,0 +1,22 @@
+package com.odeyalo.kyrie.core.oauth2.support.grant.callbacks;
+
+import lombok.Data;
+
+/**
+ * Callback that will be invoked when the user authentication is failed
+ */
+@FunctionalInterface
+public interface AuthenticationFailedCallback extends Callback<AuthenticationFailedCallback.AuthenticationFailedCallbackData> {
+
+    void handleAuthenticationFailedCallback(AuthenticationFailedCallbackData data);
+
+    @Override
+    default void handleCallback(AuthenticationFailedCallbackData data) {
+        handleAuthenticationFailedCallback(data);
+    }
+
+    @Data
+    class AuthenticationFailedCallbackData {
+        private final String reason;
+    }
+}
