47f8de625f865b785f7610b3f2f6e329019a876f
Odeyalo <theprogrammerinfuture@gmail.com>
2023-02-03 15:59:36 +0200

    Added null check

--- src/main/java/com/odeyalo/kyrie/core/oauth2/flow/support/DefaultRedirectableOauth2FlowHandlerFacade.java
+++ src/main/java/com/odeyalo/kyrie/core/oauth2/flow/support/DefaultRedirectableOauth2FlowHandlerFacade.java
@@ -3,8 +3,10 @@ package com.odeyalo.kyrie.core.oauth2.flow.support;
 import com.odeyalo.kyrie.core.Oauth2User;
 import com.odeyalo.kyrie.core.authorization.AuthorizationRequest;
 import com.odeyalo.kyrie.core.oauth2.Oauth2Token;
+import com.odeyalo.kyrie.core.oauth2.flow.Oauth2FlowHandler;
 import com.odeyalo.kyrie.core.oauth2.flow.Oauth2FlowHandlerFactory;
 import com.odeyalo.kyrie.core.oauth2.support.RedirectUrlCreationServiceFactory;
+import com.odeyalo.kyrie.exceptions.InvalidGrantOauth2Exception;
 import com.odeyalo.kyrie.exceptions.Oauth2Exception;
 import org.springframework.stereotype.Component;
 
@@ -23,7 +25,11 @@ public class DefaultRedirectableOauth2FlowHandlerFacade implements RedirectableO
 
     @Override
     public String handleFlow(Oauth2User user, AuthorizationRequest request) throws Oauth2Exception {
-        Oauth2Token oauth2Token = oauth2FlowHandlerFactory.getOauth2FlowHandler(request).handleFlow(request, user);
+        Oauth2FlowHandler flowHandler = oauth2FlowHandlerFactory.getOauth2FlowHandler(request);
+        if (flowHandler == null) {
+            throw new InvalidGrantOauth2Exception("The grant type is null and request cannot be performed properly!", "The grant type is null and request cannot be performed properly!");
+        }
+        Oauth2Token oauth2Token = flowHandler.handleFlow(request, user);
 
         return redirectUrlCreationServiceFactory.getRedirectUrlCreationService(request).createRedirectUrl(request, oauth2Token);
     }
