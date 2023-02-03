8e7e321f8c1fa2da6951a0df8b5790bac60b1c12
Odeyalo <theprogrammerinfuture@gmail.com>
2023-02-03 16:00:28 +0200

    Added TestPropertySource

--- src/test/java/com/odeyalo/kyrie/AbstractIntegrationTest.java
+++ src/test/java/com/odeyalo/kyrie/AbstractIntegrationTest.java
@@ -2,11 +2,13 @@ package com.odeyalo.kyrie;
 
 import com.odeyalo.kyrie.config.annotation.EnableKyrieAuthorizationServer;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 /**
  * Used to cache Spring application context and avoid context recreating to boost performance
  */
 @SpringBootTest
 @EnableKyrieAuthorizationServer
+@TestPropertySource(locations = "classpath:application-test.properties")
 public class AbstractIntegrationTest {
 }
