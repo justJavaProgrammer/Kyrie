8360bf4e0fcea7723104c473c2983484866616a1
Odeyalo <theprogrammerinfuture@gmail.com>
2023-02-03 15:13:26 +0200

    Now extends AbstractConsentPageHandler class

--- src/main/java/com/odeyalo/kyrie/core/oauth2/support/consent/DefaultKyrieHtmlFormConsentPageHandler.java
+++ src/main/java/com/odeyalo/kyrie/core/oauth2/support/consent/DefaultKyrieHtmlFormConsentPageHandler.java
@@ -4,33 +4,39 @@ import com.odeyalo.kyrie.core.Oauth2User;
 import com.odeyalo.kyrie.core.authorization.AuthorizationRequest;
 import com.odeyalo.kyrie.core.oauth2.flow.support.RedirectableOauth2FlowHandlerFacade;
 import com.odeyalo.kyrie.core.oauth2.support.Oauth2Constants;
+import com.odeyalo.kyrie.core.support.web.TemporaryRequestAttributesRepository;
 import com.odeyalo.kyrie.support.html.DefaultTemplateResolver;
 import com.odeyalo.kyrie.support.html.TemplateResolver;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import io.jsonwebtoken.lang.Assert;
 import org.springframework.ui.ExtendedModelMap;
-import org.springframework.util.Assert;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
- * ConsentPageHandler that used to handle ONLY html form in consent page.
+ * <p>ConsentPageHandler that used to handle ONLY html form in consent page.</p>
+ * The implementation approves the consent only if the request contains {@link #approvedFormParameter} in parameters, otherwise the consent is denied.
+ *
+ * The {@link #approvedFormParameter} can be customized through {@link #setApprovedFormParameter(String)} method
  */
-@Component
-public class DefaultKyrieHtmlFormConsentPageHandler implements ConsentPageHandler {
-    public static final String AUTHENTICATED_USER_SESSION_ATTRIBUTE_NAME = "authenticated_user";
-    @Autowired
-    private TemplateResolver templateResolver;
-    @Autowired
-    private RedirectableOauth2FlowHandlerFacade redirectableOauth2FlowHandlerFacade;
+public class DefaultKyrieHtmlFormConsentPageHandler extends AbstractConsentPageHandler {
+    private final RedirectableOauth2FlowHandlerFacade redirectableOauth2FlowHandlerFacade;
+    private final TemporaryRequestAttributesRepository temporaryRequestAttributesRepository;
+    private final TemplateResolver templateResolver;
 
-    public static final String APPROVED_FORM_PARAMETER = "approved";
-    public static final String DENIED_FORM_PARAMETER = "denied";
+    private String approvedFormParameter = "approved";
+
+
+    public DefaultKyrieHtmlFormConsentPageHandler(RedirectableOauth2FlowHandlerFacade redirectableOauth2FlowHandlerFacade,
+                                                  TemporaryRequestAttributesRepository temporaryRequestAttributesRepository,
+                                                  TemplateResolver templateResolver) {
+        this.redirectableOauth2FlowHandlerFacade = redirectableOauth2FlowHandlerFacade;
+        this.temporaryRequestAttributesRepository = temporaryRequestAttributesRepository;
+        this.templateResolver = templateResolver;
+    }
 
     /**
      * Store the current user in the session and return the consent page that was resolved by {@link TemplateResolver}
@@ -41,10 +47,10 @@ public class DefaultKyrieHtmlFormConsentPageHandler implements ConsentPageHandle
      */
     @Override
     public ModelAndView getConsentPage(Oauth2User user, AuthorizationRequest authorizationRequest, HttpServletRequest request) {
-        HttpSession session = request.getSession();
-        Assert.state(session != null, "The session must be presented for the request");
-
-        session.setAttribute(AUTHENTICATED_USER_SESSION_ATTRIBUTE_NAME, user);
+        // Save the user only if the request does not contain AUTHENTICATED_USER_ATTRIBUTE_NAME attribute to avoid saving twice
+        if (temporaryRequestAttributesRepository.get(request, AUTHENTICATED_USER_ATTRIBUTE_NAME) == null) {
+            temporaryRequestAttributesRepository.save(request, AUTHENTICATED_USER_ATTRIBUTE_NAME, user);
+        }
 
         ExtendedModelMap model = new ExtendedModelMap();
 
@@ -53,33 +59,33 @@ public class DefaultKyrieHtmlFormConsentPageHandler implements ConsentPageHandle
         return templateResolver.getTemplate(DefaultTemplateResolver.CONSENT_VIEW_TEMPLATE_TYPE, model);
     }
 
+    /**
+     * The method implementation that checks if the {@link #approvedFormParameter} is presented in parameter, if so, then the consent is approved,
+     * otherwise user rejected consent
+     * @param authorizationRequest - AuthorizationRequest associated with current authentication session
+     * @param request - current request
+     * @return - {@link ConsentResult#approved(Oauth2User)} if the APPROVED_FORM_PARAMETER is presented in request parameters, {@link ConsentResult#denied(Oauth2User)})} otherwise
+     */
     @Override
-    public void handleSubmit(AuthorizationRequest authorizationRequest, HttpServletRequest request, HttpServletResponse response) {
-        String approved = request.getParameter(APPROVED_FORM_PARAMETER);
-        Oauth2User user = (Oauth2User) request.getSession().getAttribute("user");
+    ConsentResult isApproved(AuthorizationRequest authorizationRequest, HttpServletRequest request) {
+        String approved = request.getParameter(approvedFormParameter);
 
-        if (approved != null) {
-            // User approved access
-            String redirectUri = redirectableOauth2FlowHandlerFacade.handleFlow(user, authorizationRequest);
-            sendRedirect(response, redirectUri);
-            return;
-        }
-        // User denied the access
-        String redirectUri = UriComponentsBuilder.fromUriString(authorizationRequest.getRedirectUrl()).queryParam(Oauth2Constants.ERROR_PARAMETER_NAME, "access_denied").toUriString();
+        Oauth2User user = temporaryRequestAttributesRepository.get(request, AUTHENTICATED_USER_ATTRIBUTE_NAME, Oauth2User.class);
 
+        return approved != null ? ConsentResult.approved(user) : ConsentResult.denied(user);
+    }
+
+    @Override
+    public void onAccessApproved(Oauth2User user, AuthorizationRequest authorizationRequest, HttpServletRequest request, HttpServletResponse response) {
+        String redirectUri = redirectableOauth2FlowHandlerFacade.handleFlow(user, authorizationRequest);
         sendRedirect(response, redirectUri);
     }
 
     @Override
-    public void onAccessApproved(AuthorizationRequest authorizationRequest, HttpServletRequest request, HttpServletResponse response) {
-
+    public void onAccessDenied(Oauth2User user, AuthorizationRequest authorizationRequest, HttpServletRequest request, HttpServletResponse response) {
+        String redirectUri = UriComponentsBuilder.fromUriString(authorizationRequest.getRedirectUrl()).queryParam(Oauth2Constants.ERROR_PARAMETER_NAME, "access_denied").toUriString();
+        sendRedirect(response, redirectUri);
     }
-
-    @Override
-    public void onAccessDenied(AuthorizationRequest authorizationRequest, HttpServletRequest request, HttpServletResponse response) {
-
-    }
-
 
     private void sendRedirect(HttpServletResponse response, String location) {
         try {
@@ -87,5 +93,14 @@ public class DefaultKyrieHtmlFormConsentPageHandler implements ConsentPageHandle
         } catch (IOException exception) {
             exception.printStackTrace();
         }
+    }
+
+    public String getApprovedFormParameter() {
+        return approvedFormParameter;
+    }
+
+    public void setApprovedFormParameter(String approvedFormParameter) {
+        Assert.notNull(approvedFormParameter, "Approved parameter must be not null!");
+        this.approvedFormParameter = approvedFormParameter;
     }
 }
