02acc53935da18d98b1fd16ba10bca5118914c97
Odeyalo <theprogrammerinfuture@gmail.com>
2023-02-03 16:00:02 +0200

    Rewritten to java config, added exception resolving

--- src/main/java/com/odeyalo/kyrie/core/oauth2/support/grant/DefaultRedirectableAuthenticationGrantHandlerFacade.java
+++ src/main/java/com/odeyalo/kyrie/core/oauth2/support/grant/DefaultRedirectableAuthenticationGrantHandlerFacade.java
@@ -5,7 +5,8 @@ import com.odeyalo.kyrie.core.authentication.Oauth2UserAuthenticationService;
 import com.odeyalo.kyrie.core.authorization.AuthorizationRequest;
 import com.odeyalo.kyrie.core.oauth2.flow.support.RedirectableOauth2FlowHandlerFacade;
 import com.odeyalo.kyrie.core.sso.RememberMeService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.odeyalo.kyrie.exceptions.Oauth2ErrorType;
+import com.odeyalo.kyrie.exceptions.Oauth2Exception;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,16 +15,16 @@ import javax.servlet.http.HttpServletResponse;
  * Default {@link AbstractRedirectableAuthenticationGrantHandlerFacade} implementation, that authenticate the user and redirect user to client application WITHOUT consent page
  *
  */
-//@Component
 public class DefaultRedirectableAuthenticationGrantHandlerFacade extends AbstractRedirectableAuthenticationGrantHandlerFacade {
     private final RedirectableOauth2FlowHandlerFacade redirectableOauth2FlowHandlerFacade;
-//todo: Rewrite to Java config
-    @Autowired
-    private RememberMeService rememberMeService;
+    private final RememberMeService rememberMeService;
 
-    public DefaultRedirectableAuthenticationGrantHandlerFacade(Oauth2UserAuthenticationService oauth2UserAuthenticationService, RedirectableOauth2FlowHandlerFacade redirectableOauth2FlowHandlerFacade){
+    public DefaultRedirectableAuthenticationGrantHandlerFacade(Oauth2UserAuthenticationService oauth2UserAuthenticationService,
+                                                               RedirectableOauth2FlowHandlerFacade redirectableOauth2FlowHandlerFacade,
+                                                               RememberMeService rememberMeService){
         super(oauth2UserAuthenticationService);
         this.redirectableOauth2FlowHandlerFacade = redirectableOauth2FlowHandlerFacade;
+        this.rememberMeService = rememberMeService;
     }
 
     @Override
@@ -32,9 +33,14 @@ public class DefaultRedirectableAuthenticationGrantHandlerFacade extends Abstrac
             String redirectUri = redirectableOauth2FlowHandlerFacade.handleFlow(user, authorizationRequest);
             rememberMeService.rememberMe(user, request, response);
             return HandleResult.success(true, redirectUri);
-        } catch (Exception ex) {
+        } catch (Oauth2Exception ex) {
             logger.error("Failed to create redirect uri", ex);
-            return HandleResult.failed(true, "REDIRECT_URI_CREATION_ERROR_TYPE_TYPE");
+            Oauth2ErrorType errorType = ex.getErrorType();
+
+            if (Oauth2ErrorType.INVALID_GRANT.equals(errorType)) {
+                return HandleResult.UNSUPPORTED_GRANT_TYPE_HANDLE_RESULT;
+            }
+            return HandleResult.failed(true, errorType.getErrorName());
         }
     }
 }
