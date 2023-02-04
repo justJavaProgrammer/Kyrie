ccb0cdd2bc2e2b81f3adb7c265619611964fd5b6
Odeyalo <theprogrammerinfuture@gmail.com>
2023-02-04 17:41:23 +0200

    Removed RememberMe

--- src/main/java/com/odeyalo/kyrie/core/oauth2/support/grant/DefaultRedirectableAuthenticationGrantHandlerFacade.java
+++ src/main/java/com/odeyalo/kyrie/core/oauth2/support/grant/DefaultRedirectableAuthenticationGrantHandlerFacade.java
@@ -4,7 +4,6 @@ import com.odeyalo.kyrie.core.Oauth2User;
 import com.odeyalo.kyrie.core.authentication.Oauth2UserAuthenticationService;
 import com.odeyalo.kyrie.core.authorization.AuthorizationRequest;
 import com.odeyalo.kyrie.core.oauth2.flow.support.RedirectableOauth2FlowHandlerFacade;
-import com.odeyalo.kyrie.core.sso.RememberMeService;
 import com.odeyalo.kyrie.exceptions.Oauth2ErrorType;
 import com.odeyalo.kyrie.exceptions.Oauth2Exception;
 
@@ -17,21 +16,17 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class DefaultRedirectableAuthenticationGrantHandlerFacade extends AbstractRedirectableAuthenticationGrantHandlerFacade {
     private final RedirectableOauth2FlowHandlerFacade redirectableOauth2FlowHandlerFacade;
-    private final RememberMeService rememberMeService;
 
     public DefaultRedirectableAuthenticationGrantHandlerFacade(Oauth2UserAuthenticationService oauth2UserAuthenticationService,
-                                                               RedirectableOauth2FlowHandlerFacade redirectableOauth2FlowHandlerFacade,
-                                                               RememberMeService rememberMeService){
+                                                               RedirectableOauth2FlowHandlerFacade redirectableOauth2FlowHandlerFacade){
         super(oauth2UserAuthenticationService);
         this.redirectableOauth2FlowHandlerFacade = redirectableOauth2FlowHandlerFacade;
-        this.rememberMeService = rememberMeService;
     }
 
     @Override
     protected HandleResult doHandleGrant(Oauth2User user, AuthorizationRequest authorizationRequest, HttpServletRequest request, HttpServletResponse response) {
         try {
             String redirectUri = redirectableOauth2FlowHandlerFacade.handleFlow(user, authorizationRequest);
-            rememberMeService.rememberMe(user, request, response);
             return HandleResult.success(true, redirectUri);
         } catch (Oauth2Exception ex) {
             logger.error("Failed to create redirect uri", ex);
