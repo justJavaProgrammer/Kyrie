7145a1969ed5bb84a5639e9d562e6a6abc727e8d
Odeyalo <theprogrammerinfuture@gmail.com>
2023-02-04 17:37:05 +0200

    Added logger

--- src/main/java/com/odeyalo/kyrie/core/events/listener/domain/Oauth2UserStoreUserLoginAuthenticationGrantedKyrieEventListener.java
+++ src/main/java/com/odeyalo/kyrie/core/events/listener/domain/Oauth2UserStoreUserLoginAuthenticationGrantedKyrieEventListener.java
@@ -4,19 +4,20 @@ import com.odeyalo.kyrie.core.Oauth2User;
 import com.odeyalo.kyrie.core.events.authentication.UserLoginAuthenticationGrantedKyrieEvent;
 import com.odeyalo.kyrie.core.events.listener.UserLoginAuthenticationGrantedKyrieEventListener;
 import com.odeyalo.kyrie.core.support.web.TemporaryRequestAttributesRepository;
-import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 
 /**
- * Domain listener in Kyrie, that save the user in TemporaryRequestAttributesRepository after authentication
+ * Domain listener in Kyrie, that saves the user in TemporaryRequestAttributesRepository after authentication
  */
-@Component
 public class Oauth2UserStoreUserLoginAuthenticationGrantedKyrieEventListener implements UserLoginAuthenticationGrantedKyrieEventListener {
     public static final String AUTHENTICATED_USER_ATTRIBUTE_NAME = "authenticated_user";
     private final TemporaryRequestAttributesRepository temporaryRequestAttributesRepository;
+    private final Logger logger = LoggerFactory.getLogger(Oauth2UserStoreUserLoginAuthenticationGrantedKyrieEventListener.class);
 
     public Oauth2UserStoreUserLoginAuthenticationGrantedKyrieEventListener(TemporaryRequestAttributesRepository temporaryRequestAttributesRepository) {
         this.temporaryRequestAttributesRepository = temporaryRequestAttributesRepository;
@@ -31,5 +32,7 @@ public class Oauth2UserStoreUserLoginAuthenticationGrantedKyrieEventListener imp
         }
         HttpServletRequest request = requestAttributes.getRequest();
         temporaryRequestAttributesRepository.save(request, AUTHENTICATED_USER_ATTRIBUTE_NAME, oauth2User);
+
+        this.logger.debug("Saved the oauth2 user: {} with key: {}", oauth2User, AUTHENTICATED_USER_ATTRIBUTE_NAME);
     }
 }
