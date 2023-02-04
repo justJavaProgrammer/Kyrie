c044bc34393d0f4b116b8f608878daf899176921
Odeyalo <theprogrammerinfuture@gmail.com>
2023-02-04 17:46:42 +0200

    removed request.getSession().setAttribute()

--- src/main/java/com/odeyalo/kyrie/core/oauth2/support/grant/ConsentPageConfigurableRedirectableAuthenticationGrantHandlerFacade.java
+++ src/main/java/com/odeyalo/kyrie/core/oauth2/support/grant/ConsentPageConfigurableRedirectableAuthenticationGrantHandlerFacade.java
@@ -4,8 +4,6 @@ import com.odeyalo.kyrie.config.configurers.Oauth2ServerEndpointsConfigurer;
 import com.odeyalo.kyrie.core.Oauth2User;
 import com.odeyalo.kyrie.core.authentication.Oauth2UserAuthenticationService;
 import com.odeyalo.kyrie.core.authorization.AuthorizationRequest;
-import com.odeyalo.kyrie.core.oauth2.support.consent.ConsentPageHandler;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,20 +14,15 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class ConsentPageConfigurableRedirectableAuthenticationGrantHandlerFacade extends AbstractRedirectableAuthenticationGrantHandlerFacade {
     private final Oauth2ServerEndpointsConfigurer.Oauth2ServerEndpointsInfo endpointsInfo;
-    private final ConsentPageHandler consentPageHandler;
 
-    public ConsentPageConfigurableRedirectableAuthenticationGrantHandlerFacade(Oauth2UserAuthenticationService oauth2UserAuthenticationService, Oauth2ServerEndpointsConfigurer.Oauth2ServerEndpointsInfo endpointsInfo, ConsentPageHandler consentPageHandler) {
+    public ConsentPageConfigurableRedirectableAuthenticationGrantHandlerFacade(Oauth2UserAuthenticationService oauth2UserAuthenticationService,
+                                                                               Oauth2ServerEndpointsConfigurer.Oauth2ServerEndpointsInfo endpointsInfo) {
         super(oauth2UserAuthenticationService);
         this.endpointsInfo = endpointsInfo;
-        this.consentPageHandler = consentPageHandler;
     }
 
     @Override
     protected HandleResult doHandleGrant(Oauth2User user, AuthorizationRequest authorizationRequest, HttpServletRequest request, HttpServletResponse response) {
-
-        ModelAndView consentPage = consentPageHandler.getConsentPage(user, authorizationRequest, request);
-
-        request.getSession().setAttribute("user", user);
 
         String redirectUri = UriComponentsBuilder.fromPath(endpointsInfo.getConsentPageEndpointName()).build().toUriString();
 
